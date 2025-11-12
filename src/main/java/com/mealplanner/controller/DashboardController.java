fc5b02de169a79aaa96f8fdcacc9b751bd83e312
TMGan <gangadeentim@gmail.com>
2025-11-11 23:32:23 -0500

    added jakarta import

--- src/main/java/com/mealplanner/controller/DashboardController.java
+++ src/main/java/com/mealplanner/controller/DashboardController.java
@@ -13,6 +13,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class DashboardController {
